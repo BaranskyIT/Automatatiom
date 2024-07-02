b3184433530758e11700506d648b055550c4aa28
BaranskyIT <baranskyit@gmail.com>
2024-07-02 22:55:37 +1000

    изменение домашнего задания

--- lesson1/lesson_1_task_2.py
+++ lesson1/lesson_1_task_2.py
@@ -1,3 +1,4 @@
 my_age = 27
+my_age += 3
 
 print(my_age)
--- lesson1/lesson_1_task_7.py
+++ lesson1/lesson_1_task_7.py
@@ -1,5 +1,5 @@
 def print_num(num):
-    print(num)
+    print(num, end="")
 
 print_num(8)
 print_num(8)
@@ -8,7 +8,7 @@ print_num(0)
 print_num(5)
 print_num(5)
 print_num(5)
-print_num(5)
 print_num(3)
 print_num(5)
 print_num(3)
+print_num(5)
